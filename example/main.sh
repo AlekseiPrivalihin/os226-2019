5d06f1e81a70e25b2011646a85d134fbaa65428f
AlekseiPrivalihin <aleksei.privalihin@gmail.com>
2019-09-07 10:11:50 +0100

    HW1 by Aleksei Privalihin

--- example/main.sh
+++ example/main.sh
@@ -1,2 +1,31 @@
 #!/bin/sh
 
+maxNumber=0
+maxID="Error! No input was given!"
+curNumber=0
+curID=0
+
+for studentID in `sort`
+do
+	if [ "$studentID" = "$curID" ]
+	then
+		curNumber=$(expr $curNumber + 1)
+	else
+		if [ $curNumber -gt $maxNumber ]
+		then
+			maxNumber=$curNumber
+			maxID="$curID"
+		fi
+
+		curID="$studentID"
+		curNumber=1
+	fi
+done
+
+if [ $curNumber -gt $maxNumber ]
+then
+	maxNumber=$curNumber
+	maxID="$curID"
+fi
+
+echo "$maxID"
